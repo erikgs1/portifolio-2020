edc070f03740b2c8145ca17c81126a20593ddea2
Erik Gomes <erik.contato15@gmail.com>
2020-10-13 11:49:02 -0300

    Adicionei estilo simples
    coloquei um hover nos box. e um border radius na imagem da bio, linkei o main com o banner do header

---
+++ Portifolio/js/script.js
@@ -0,0 +1,8 @@
+// init controller
+var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+// build scenes
+new ScrollMagic.Scene({triggerElement: "#parallax1"})
+    .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+    .addIndicators()
+    .addTo(controller);
